da6c84913e0958a7b7a742bfd2613e8df0f99581
chentiande <chentiande@boco.com.cn>
2019-07-13 09:50:47 +0800

    完善提示，修正了字符集

--- DbUnloadExcel.go
+++ DbUnloadExcel.go
@@ -6,6 +6,7 @@ import (
     "log"
     "os"
     "strconv"
+    "strings"
     "io/ioutil"
 
     _ "github.com/mattn/go-oci8"
@@ -13,11 +14,13 @@ import (
 )
 
 func query() {
-    os.Setenv("NLS_LANG", "AMERICAN_AMERICA.AL32UTF8")
+    os.Setenv("NLS_LANG", "Simplified Chinese_China.AL32UTF8")
     log.SetFlags(log.Lshortfile | log.LstdFlags)
     f := excelize.NewFile()
     //db, err := sql.Open("oci8", "system/system@192.168.1.86:1521/ORCLCDB")
-    db, err := sql.Open("oci8", os.Args[1])
+     str:=os.Args[1]
+     str=strings.Replace(str, "\\", "", -1)
+    db, err := sql.Open("oci8", str)
     if err != nil {
         log.Fatal(err)
     }
@@ -27,7 +30,8 @@ func query() {
         fmt.Println("ioutil ReadFile error: ", err)
         return
     }
-    rows, err := db.Query(string(b))
+    fmt.Printf("sql=%v",string(b))
+    rows, err := db.Query(strings.Replace(string(b),";","",-1))
     if err != nil {
         log.Fatal(err)
     }
@@ -55,24 +59,31 @@ func query() {
       f.SetCellValue("Sheet1", string(65+m)+strconv.Itoa(j), result[m])
     }
         j++
-     if j%10000==0 {
-       fmt.Println("导出"+strconv.Itoa(j)+"行")
+     if (j-2)%10000==0 {
+       fmt.Println("导出"+strconv.Itoa(j-2)+"行")
        
       }
 //        fmt.Printf("%s\t%s\n", result[0],result[1])
     }
-      fmt.Println("总数据量为"+strconv.Itoa(j)+"行")
+      fmt.Println("总数据量为"+strconv.Itoa(j-2)+"行")
     rows.Close()
        fmt.Println("开始生成EXCEL，请耐心等待.....")
     err = f.SaveAs(os.Args[3])
     if err != nil {
         fmt.Println(err)
-    }
+    }else{
+       fmt.Println("导出数据成功")    
+         }
 }
 
 func main() {
+     if len(os.Args)!=4{
+     fmt.Println("参数设置不正确,需要包含三个参数:连接串,包含sql的文件名,保存的excel文件名")
+     fmt.Println("./DbUnloadExcel user/passwd@ip:port/service_name objects.sql object.xlsx")
+     fmt.Println("如有其它问题请联系chentiande@boco.com.cn")
+     }else {
+  
      fmt.Println("开始导出数据......")
     query()
-      fmt.Println("导出数据成功")
-     
+    } 
 }
